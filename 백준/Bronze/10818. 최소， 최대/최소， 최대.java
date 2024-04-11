5debb40855c9e17e7c593af379cff8f799c3849c
IcedCafeMocha <162860613+IcedCafeMocha@users.noreply.github.com>
2024-04-11 22:31:15 +0900

    [Bronze III] Title: 최소, 최대, Time: 516 ms, Memory: 106280 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,18 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int MIN = Integer.MAX_VALUE;
+		int MAX = Integer.MIN_VALUE;
 		String[] input;
 		int N = Integer.parseInt(br.readLine());
-		int[] temp = new int[N];
+		int temp;
 		input = br.readLine().split(" ");
-		for(int i=0; i<N; i++)
-			temp[i] = Integer.parseInt(input[i]);
-		Arrays.sort(temp);
-		System.out.println(temp[0]+" "+temp[N-1]);
+		for (int i = 0; i < N; i++) {
+			temp = Integer.parseInt(input[i]);
+			if (temp < MIN)
+				MIN = temp;
+			if (temp > MAX)
+				MAX = temp;
+		}
+		System.out.println(MIN + " " + MAX);
 	}
 }
