1a66938935afc51152b85a1a07c4524d350be15a
IcedCafeMocha <162860613+IcedCafeMocha@users.noreply.github.com>
2024-04-22 21:35:57 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1648 ms, Memory: 116452 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int[] ary = new int[N];
+		for(int i=0; i<N; i++)
+			ary[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(ary);
+		for(int i=0; i<N; i++)
+			bw.write(ary[i]+"\n");
+		bw.flush();
+		bw.close();
+	}
+}
