f777608b8a101320a3d3f47d21f7b25c86f7cfb4
IcedCafeMocha <162860613+IcedCafeMocha@users.noreply.github.com>
2024-05-18 00:26:24 +0900

    [Bronze V] Title: A+B - 7, Time: 164 ms, Memory: 16156 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input;
+		int T = Integer.parseInt(br.readLine());
+		for(int testcase=1; testcase<=T; testcase++) {
+			input = br.readLine().split(" ");
+			bw.write("Case #"+testcase+": "+(Integer.parseInt(input[0])+Integer.parseInt(input[1]))+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
